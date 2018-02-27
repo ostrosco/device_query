70eef64b866d1716ee57f74db8ecdd0701c152e4
ostrosco <ostrosco@fastmail.fm>
2018-02-26 18:56:04 -0700

    Removing needless libraries from build script

--- build.rs
+++ build.rs
@@ -17,36 +17,17 @@ use std::path::Path;
 
 #[cfg(target_os = "linux")]
 fn main() {
-    let libraries = [
-        "xext",
-        "gl",
-        "xcursor",
-        "xxf86vm",
-        "xft",
-        "xinerama",
-        "xi",
-        "x11",
-        "xlib_xcb",
-        "xmu",
-        "xrandr",
-        "xtst",
-        "xrender",
-        "xscrnsaver",
-        "xt",
-    ];
-
     let mut config = String::new();
-    for lib in libraries.iter() {
-        let libdir = match pkg_config::get_variable(lib, "libdir") {
-            Ok(libdir) => format!("Some(\"{}\")", libdir),
-            Err(_) => "None".to_string(),
-        };
-        config.push_str(&format!(
-            "pub const {}: Option<&'static str> = {};\n",
-            lib,
-            libdir
-        ));
-    }
+    let libdir = match pkg_config::get_variable("x11", "libdir") {
+        Ok(libdir) => format!("Some(\"{}\")", libdir),
+        Err(_) => "None".to_string(),
+    };
+    config.push_str(&format!(
+        "pub const {}: Option<&'static str> = {};\n",
+        "x11",
+        libdir
+    ));
+
     let config = format!("pub mod config {{ pub mod libdir {{\n{}}}\n}}", config);
     let out_dir = env::var("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("config.rs");
