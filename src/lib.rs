7bf296b5ce6588e6c768bc4d63af03f2786efe27
ostrosco <ostrosco@fastmail.fm>
2018-02-26 22:20:55 -0700

    Adding Windows support for querying mouse position
    - Windows support now exists for getting the mouse position
    - Fixed some build issues in Windows where X11 would be build
      erronousely

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 #[cfg(target_os = "linux")]
 pub mod linux;
+#[cfg(target_os = "linux")]
 pub use linux::MouseCoords;
+
+#[cfg(target_os = "windows")]
+pub mod windows;
+#[cfg(target_os = "windows")]
+pub use windows::MouseCoords;
 
 pub trait MouseQuery {
     fn get_coords(&self) -> (i32, i32);
---
+++ src/windows/mod.rs
@@ -0,0 +1,24 @@
+extern crate winapi;
+extern crate user32;
+
+use windows::winapi::windef::POINT;
+use windows::user32::GetCursorPos;
+
+pub struct MouseCoords;
+
+impl MouseCoords {
+    pub fn new() -> MouseCoords {
+        MouseCoords {}
+    }
+
+    pub fn query_pointer(&self) -> (i32, i32) {
+        unsafe {
+            let point = &mut POINT {x: 0, y: 0};
+            if GetCursorPos(point) != 0 {
+                ((*point).x, (*point).y)
+            } else {
+                (0, 0)
+            }
+        }
+    }
+}
