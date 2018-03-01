4cab061de2de654b7843642b4ea712dc4f5b90e5
ostrosco <ostrosco@fastmail.fm>
2018-02-28 23:19:39 -0700

    Initial testing on Linux keyboard input, making progress

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+pub mod keymap;
+
 #[cfg(target_os = "linux")]
 pub mod linux;
 #[cfg(target_os = "linux")]
@@ -10,10 +12,16 @@ pub use windows::MouseCoords;
 
 pub trait MouseQuery {
     fn get_coords(&self) -> (i32, i32);
+    fn get_keys(&self) -> ();
 }
 
 impl MouseQuery for MouseCoords {
     fn get_coords(&self) -> (i32, i32) {
         self.query_pointer()
+    }
+
+    fn get_keys(&self) -> () {
+        self.query_keymap();
+        ()
     }
 }
--- src/linux/mod.rs
+++ src/linux/mod.rs
@@ -2,6 +2,8 @@ extern crate x11;
 
 use linux::x11::xlib;
 use std::ptr;
+use keymap::Keycode;
+use std::slice;
 
 pub struct MouseCoords {
     display: *mut xlib::Display,
@@ -10,6 +12,7 @@ pub struct MouseCoords {
 impl MouseCoords {
     pub fn new() -> MouseCoords {
         unsafe {
+            xlib::XInitThreads();
             let disp = xlib::XOpenDisplay(ptr::null());
             MouseCoords {
                 display: disp,
@@ -32,6 +35,18 @@ impl MouseCoords {
                                          &mut root_x, &mut root_y,
                                          &mut win_x, &mut win_y, &mut mask_return);
             (win_x, win_y)
+        }
+    }
+
+    pub fn query_keymap(&self) -> Vec<Keycode> {
+        unsafe {
+            let keymap: *mut i8 = [0; 32].as_mut_ptr();
+            xlib::XQueryKeymap(self.display, keymap);
+            for ix in slice::from_raw_parts(keymap, 32).iter() {
+                print!("{}", ix);
+            }
+            println!("");
+            vec![]
         }
     }
 }
