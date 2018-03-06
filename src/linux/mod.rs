59a58f5fcf24e6f810eba3c85b5010ab8f48c2c3
ostrosco <ostrosco@fastmail.fm>
2018-03-05 22:42:30 -0700

    Adding initial support for key presses, but will likely switch to events

--- src/linux/mod.rs
+++ src/linux/mod.rs
@@ -1,6 +1,7 @@
 extern crate x11;
 
 use linux::x11::xlib;
+use linux::x11::keysym;
 use std::ptr;
 use keymap::Keycode;
 use std::slice;
@@ -14,9 +15,7 @@ impl MouseCoords {
         unsafe {
             xlib::XInitThreads();
             let disp = xlib::XOpenDisplay(ptr::null());
-            MouseCoords {
-                display: disp,
-            }
+            MouseCoords { display: disp }
         }
     }
 
@@ -31,9 +30,17 @@ impl MouseCoords {
             let mut root_return = 0;
             let mut child_return = 0;
             let mut mask_return = 0;
-            xlib::XQueryPointer( self.display, root, &mut root_return, &mut child_return, 
-                                         &mut root_x, &mut root_y,
-                                         &mut win_x, &mut win_y, &mut mask_return);
+            xlib::XQueryPointer(
+                self.display,
+                root,
+                &mut root_return,
+                &mut child_return,
+                &mut root_x,
+                &mut root_y,
+                &mut win_x,
+                &mut win_y,
+                &mut mask_return,
+            );
             (win_x, win_y)
         }
     }
@@ -41,12 +48,76 @@ impl MouseCoords {
     pub fn query_keymap(&self) -> Vec<Keycode> {
         unsafe {
             let keymap: *mut i8 = [0; 32].as_mut_ptr();
+            let mut keycodes = vec![];
             xlib::XQueryKeymap(self.display, keymap);
-            for ix in slice::from_raw_parts(keymap, 32).iter() {
-                print!("{}", ix);
+            for (ix, byte) in
+                slice::from_raw_parts(keymap, 32).iter().enumerate()
+            {
+                for bit in 0_u8..8_u8 {
+                    let bitmask = 1 << bit;
+                    if byte & bitmask != 0 {
+                        let keycode = ix as u8 * 8 + bit;
+                        let mut keysyms_per_keycode_return = 0_i32;
+                        let keysym = xlib::XGetKeyboardMapping(
+                            self.display,
+                            keycode,
+                            1,
+                            &mut keysyms_per_keycode_return,
+                        );
+
+                        for ks in slice::from_raw_parts(
+                            keysym,
+                            keysyms_per_keycode_return as usize,
+                        ).iter()
+                        {
+                            let key = self.keysym_to_key(*ks as u32);
+                            match key {
+                                Some(k) => keycodes.push(k),
+                                None => (),
+                            }
+                        }
+                    }
+                }
             }
-            println!("");
-            vec![]
+            keycodes.dedup();
+            if keycodes.len() > 0 {
+                println!("keycodes: {:?}", keycodes);
+            }
+            keycodes
+        }
+    }
+
+    fn keysym_to_key(&self, keysym: u32) -> Option<Keycode> {
+        match keysym {
+            keysym::XK_A => Some(Keycode::A),
+            keysym::XK_B => Some(Keycode::B),
+            keysym::XK_C => Some(Keycode::C),
+            keysym::XK_D => Some(Keycode::D),
+            keysym::XK_E => Some(Keycode::E),
+            keysym::XK_F => Some(Keycode::F),
+            keysym::XK_G => Some(Keycode::G),
+            keysym::XK_H => Some(Keycode::H),
+            keysym::XK_I => Some(Keycode::I),
+            keysym::XK_J => Some(Keycode::J),
+            keysym::XK_K => Some(Keycode::K),
+            keysym::XK_L => Some(Keycode::L),
+            keysym::XK_M => Some(Keycode::M),
+            keysym::XK_N => Some(Keycode::N),
+            keysym::XK_O => Some(Keycode::O),
+            keysym::XK_P => Some(Keycode::P),
+            keysym::XK_Q => Some(Keycode::Q),
+            keysym::XK_R => Some(Keycode::R),
+            keysym::XK_S => Some(Keycode::S),
+            keysym::XK_T => Some(Keycode::T),
+            keysym::XK_U => Some(Keycode::U),
+            keysym::XK_V => Some(Keycode::V),
+            keysym::XK_W => Some(Keycode::W),
+            keysym::XK_X => Some(Keycode::X),
+            keysym::XK_Y => Some(Keycode::Y),
+            keysym::XK_Z => Some(Keycode::Z),
+            keysym::XK_space => Some(Keycode::Space),
+            keysym::XK_Return => Some(Keycode::Enter),
+            _ => None,
         }
     }
 }
